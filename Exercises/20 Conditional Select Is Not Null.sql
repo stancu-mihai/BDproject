907be4eead46594f492b1eb95e3da25862e9bc06
Mihai <mihaistancu@mail.com>
2019-06-13 23:32:26 +0300

    Updates an exercise file

--- Exercises/20 Conditional Select Is Not Null.sql
+++ Exercises/20 Conditional Select Is Not Null.sql
@@ -1,6 +1,8 @@
 --Ex20: Display last name, salary and commission for each employee that earns a comission
+--Order by commission descending
 
 --Solution:
 SELECT LAST_NAME, SALARY, COMMISSION_PCT
 FROM EMPLOYEES
 WHERE COMMISSION_PCT IS NOT NULL
+ORDER BY COMMISSION_PCT DESC
