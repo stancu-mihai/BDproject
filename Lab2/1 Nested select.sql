99b32ec635441abc0a7e2416f9f0ff5a54afafa3
Mihai <mihaistancu@mail.com>
2019-06-15 22:27:38 +0300

    Adds an exercise

--- Lab2/1 Nested select.sql
+++ Lab2/1 Nested select.sql
@@ -1,7 +1,7 @@
 -- Display name, month (in letters) and hire date for all employees from the same department with Gates
 --Only employees whose name contain the letter "a" will be shown (2 solutions required). gates will be excluded.
 
---Solution1:
+--Solution:
 SELECT LAST_NAME,FIRST_NAME, to_char(HIRE_DATE,'mon') AS "Month", to_char(HIRE_DATE,'dd') AS "Date" FROM EMPLOYEES
 WHERE DEPARTMENT_ID= (
     SELECT DEPARTMENT_ID FROM EMPLOYEES WHERE LAST_NAME='Gates'
---
+++ Snippets/Wildcards.sql
@@ -0,0 +1,2 @@
+-- % - placeholder for no character, one character or multiple characters
+-- _ placeholder for one character
