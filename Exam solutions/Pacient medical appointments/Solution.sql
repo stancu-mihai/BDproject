e8acb4a01382dd94ea2b0573b11037b892f6557b
Mihai <mihaistancu@mail.com>
2019-06-23 19:12:10 +0300

    Updates dummy data

--- Exam solutions/Pacient medical appointments/Solution.sql
+++ Exam solutions/Pacient medical appointments/Solution.sql
@@ -6,6 +6,54 @@
 -- MEDIC (id, last name) 
 -- Observation: Assume that table PACIENT is dependent on table CLINIC
 
+-- Dummy data (needed only for testing, in lack of the actual database)
+CREATE TABLE PACIENT (
+  id number(9),
+  first_name varchar2(30),
+  last_name varchar2(30),
+  birth_date date not null,
+  CONSTRAINT pk_pacient PRIMARY KEY (id)
+);
+
+INSERT INTO PACIENT  (id, first_name, last_name, birth_date)
+VALUES (1, 'John', 'Smith', '23-JUN-19');
+
+INSERT INTO PACIENT  (id, first_name, last_name, birth_date)
+VALUES (2, 'Old', 'Man', '01-JAN-30');
+
+INSERT INTO PACIENT  (id, first_name, last_name, birth_date)
+VALUES (3, 'Jane', 'Smith', '05-FEB-45');
+
+CREATE TABLE CLINIC (
+  id number(9),
+  a_address varchar2(50),
+  city varchar2(30),
+  no_appointments number(9),
+  CONSTRAINT pk_clinic PRIMARY KEY (id)
+);
+
+INSERT INTO CLINIC  (id, a_address, city, no_appointments)
+VALUES (1, 'Address1', 'City1', 1);
+
+INSERT INTO CLINIC  (id, a_address, city, no_appointments)
+VALUES (2, 'Address2', 'City2', 1);
+
+INSERT INTO CLINIC  (id, a_address, city, no_appointments)
+VALUES (3, 'Address3', 'City3', 1);
+
+CREATE TABLE CABINET (
+  id_clinic number(9),
+  id_cabinet number(9),
+  speciality varchar2(50),
+  CONSTRAINT pk_cabinet PRIMARY KEY (id_cabinet)
+);
+
+CREATE TABLE MEDIC (
+  id number(9),
+  last_name varchar2(50),
+  CONSTRAINT pk_medic PRIMARY KEY (id)
+);
+
 --1. Create table APPOINTMENT and its integrity constraints
 
 CREATE TABLE APPOINTMENT (
@@ -23,32 +71,5 @@ CREATE TABLE APPOINTMENT (
   CONSTRAINT pacient_fk FOREIGN KEY (id_pacient) REFERENCES PACIENT(id)
 );
 
--- Dependent tables (needed only for testing, in lack of the actual database)
-CREATE TABLE PACIENT (
-  id number(9),
-  first_name varchar2(30),
-  last_name varchar2(30),
-  birth_date date not null,
-  CONSTRAINT pk_pacient PRIMARY KEY (id)
-);
-
-CREATE TABLE CLINIC (
-  id number(9),
-  a_address varchar2(50),
-  city varchar2(30),
-  no_appointments number(9),
-  CONSTRAINT pk_clinic PRIMARY KEY (id)
-);
-
-CREATE TABLE CABINET (
-  id_clinic number(9),
-  id_cabinet number(9),
-  speciality varchar2(50),
-  CONSTRAINT pk_cabinet PRIMARY KEY (id_cabinet)
-);
-
-CREATE TABLE MEDIC (
-  id number(9),
-  last_name varchar2(50),
-  CONSTRAINT pk_medic PRIMARY KEY (id)
-);
+-- 2.	Update CLINIC.no_appointments such that it contains 
+-- the number of appointments from the current month, for each clinic
